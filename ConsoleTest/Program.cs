4146afa377bf19cd562c442f583616fbc69fddd2
John Dixon <john.dixon@hull.ac.uk>
2019-11-17 00:22:33 +0000

    Mod back to console

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -14,12 +14,13 @@ namespace ConsoleTest
             while (true)
             {
                 Thread.Sleep(1);
-                display.DisplayInt(999999);
+                display.DisplayInt(100);
                 for (sbyte i = 0; i < 20; i++)
                 {
                     for (sbyte j = 0; j < 10; j++)
                     {
-                        pixels[i, j] = new Pixel((byte)random.Next(1, 256), (byte)random.Next(1, 256), (byte)random.Next(1, 256));
+                        //pixels[i, j] = new Pixel((byte)random.Next(1, 256), (byte)random.Next(1, 256), (byte)random.Next(1, 256));
+                        pixels[i, j] = new Pixel(0, 0, 0);
                     }
                 }
 
